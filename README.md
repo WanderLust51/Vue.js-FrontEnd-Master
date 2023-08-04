7acd37f19f9b190a9b025e38b6e586619a6aa5db
Andrés Lugo <andreslugo51@gmail.com>
2023-08-04 14:56:42 -0400

    Segunda clase de vue + vue-test-2 + readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vue
+
+Quickstart de vue:
+
+`npm init vue@latest`
+
+Luego:
+`cd Vue-test-2`
+`npm install`
+`npm run dev`
---
+++ Vue-test-2/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+	import { ref } from 'vue'
+	function saludar(event){
+		console.log(event)
+	}
+
+	function uncopy(e){
+		if(e.ctrlKey && e.key == 'c'){
+			alert('sin copiar chamo')
+			e.preventDefault()
+		}
+	}
+
+	let yea = ref('')
+
+</script>
+
+<template>
+
+	<header>
+		<img src="https://img.pokemondb.net/design/avif/header-lg.avif" alt="">
+		<nav @click.stop="console.log('Demonios')">
+			<div @click="saludar($event)">Pókemon data</div>
+			<div>Game mechanics</div>
+			<div>Pókemon games</div>
+			<div>Community/Other</div>
+			<div><form action=""><input type="text" @keydown="uncopy($event)" v-model="yea"><input type="submit" value=" " @click.prevent=""></form></div>
+		</nav>
+	</header>
+	<br><br>
+	<input type="text" @keypress="saludar($event)">
+	<h1>{{ yea }}</h1>
+
+
+</template>
+
+<style scoped>
+
+</style>
